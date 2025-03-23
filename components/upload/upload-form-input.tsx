f58eaddc9cad98cc840086765b082a1ed3bde589
dennis-mmachoene <dennismmachoene@gmail.com>
2025-03-23 14:52:13 +0200

    Upload Service Architecture

--- components/upload/upload-form-input.tsx
+++ components/upload/upload-form-input.tsx
@@ -1,16 +1,25 @@
+"use client";
 import React from "react";
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 interface UploadFormInputProps {
-    
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
-const UploadFormInput = ({
-    onSubmit
-}) => {
+const UploadFormInput = ({ onSubmit }: UploadFormInputProps) => {
   return (
-    <form className="flex flex-col gap-6">
-      <input type="file" />
+    <form onSubmit={onSubmit} className="flex flex-col gap-6">
+     <div className="flex justify-end items-center gap-1.5">
+     <Input
+        type="file"
+        id="file"
+        name='file'
+        accept="application/pdf"
+        required
+        className=""
+      />
       <Button>Upload your PDF</Button>
+     </div>
     </form>
   );
 };
--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import UploadFormInput from './upload-form-input'
+"use client";
+import { z } from "zod";
+import { Button } from "../ui/button";
+import UploadFormInput from "./upload-form-input";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid file" })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File must be less than 20MB"
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a PDF"
+    ),
+});
 
 const UploadForm = () => {
-    const handleSubmit = () => {
-        console.log('submitted')
-    }
-  return (
-    <div>
-       <UploadFormInput onSubmit={handleSubmit}/>
-    </div>
-  )
-}
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-export default UploadForm
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+    console.log("submitted");
+
+    //validate file
+    const validatedFields = schema.safeParse({ file });
+    //schema validation with zod
+    console.log(validatedFields)
+    if (!validatedFields.success) {
+      console.log(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid File"
+      );
+      return;
+    }
+
+    //upload file
+    //parse pdf using langchain
+    //save summary to db
+    //redirect to the [id] summary page
+  };
+  return (
+    <div className="felx flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
+};
+
+export default UploadForm;
